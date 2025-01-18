0116a173ff09c64f26cf901402d140a8fafc7aee
lalitm1004 <lalitm1004@gmail.com>
2025-01-18 14:18:06 +0530

    feat: 404

---
+++ frontend/src/routes/+error.svelte
@@ -0,0 +1,4 @@
+<main class={`h-dvh w-dvw flex flex-col gap-8 justify-center items-center`}>
+    <p class={`font-bespoke text-9xl`}>404</p>
+    <a class={`font-supreme apply-card px-6 py-2 rounded-full hover:bg-neutral-800/70`} href="/">Home</a>
+</main>
